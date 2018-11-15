4f43eeee30e295788f526478398af961bd0b7f42
Will Herring <herring.will02@Yahoo.com>
2018-11-15 12:31:55 -0800

    11/15/18 - Will

---
+++ PayrollProject/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PayrollProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MIS372Entities db = new MIS372Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            SearchGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void SearchShowButton_Click(object sender, RoutedEventArgs e)
+        {
+           if (SearchGrid.Visibility == Visibility.Visible)
+            {
+                SearchGrid.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                SearchGrid.Visibility = Visibility.Visible;
+            }   
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int? employeeID = Int32.TryParse(IDTxt.Text, out var tempVal) ? tempVal : (int?)null;
+            string firstName = FirstNameTxT.Text.NullIfWhiteSpace();
+            string lastName = LastNameTxT.Text.NullIfWhiteSpace();
+            List<GetEmployeePayments_Result2> employee = db.GetEmployeePayments(firstName, lastName, employeeID).ToList();
+            EmployeeDetailGrid.ItemsSource = employee;
+            EmployeeDetailGrid.CanUserSortColumns = true;
+            foreach (var column in EmployeeDetailGrid.Columns)
+            {
+                column.Width = new DataGridLength(EmployeeDetailGrid.ActualWidth / EmployeeDetailGrid.Columns.Count, DataGridLengthUnitType.Pixel);
+            }
+        }
+
+       
+    }
+}
---
+++ TimeLogger/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeLogger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PayrollModel db = new PayrollModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoginGrid.Visibility = Visibility.Hidden;
+            LogoutGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            int? employeeID = Int32.TryParse(LoginTxt.Text, out var tempVal) ? tempVal : (int?)null;
+            db.Login(employeeID);
+            string employeeFirst = db.Employees.Find(employeeID).FirstName;
+            MessageBoxResult result = MessageBox.Show("Successfully Logged " + employeeFirst + " In", "Confirmation", MessageBoxButton.OK);
+            LoginTxt.Clear();
+            LoginGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void LoginShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LogoutGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int? employeeID = Int32.TryParse(LogoutTxt.Text, out var tempVal) ? tempVal : (int?)null;
+            db.Logout(employeeID);
+            string employeeFirst = db.Employees.Find(employeeID).FirstName;
+            MessageBoxResult result = MessageBox.Show("Successfully Logged " + employeeFirst + " Out", "Confirmation", MessageBoxButton.OK);
+            LogoutTxt.Clear();
+            LogoutGrid.Visibility = Visibility.Hidden;
+        }
+    }
+}
