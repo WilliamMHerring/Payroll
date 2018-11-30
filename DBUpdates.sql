20a1315e90bfa8573b11e2c9b4619ef9b60acaea
groot <groot@LAPTOP-P7GPBBKF>
2018-11-30 12:32:55 -0800

    Added some SQL Procedures for Add Employees Update HourlyRate

---
+++ DBUpdates.sql
@@ -0,0 +1,76 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Geoff
+-- Create date: 
+-- Description:	
+-- =============================================
+CREATE PROCEDURE AddEmployee 
+	-- Add the parameters for the stored procedure here
+	@FirstName varchar(50), 
+	@LastName varchar(50),
+	@DepartmentID int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	Insert into Employees (FirstName, LastName, DeptartmentID) 
+	Values (@FirstName, @LastName, @DepartmentID)
+END
+GO
+
+
+
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE UpdatePayRate
+	-- Add the parameters for the stored procedure here
+	@EmployeeID int,
+	@Rate decimal(18,2)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    Update HourlyRates Set HourlyRate = @Rate
+	where EmployeeID = @EmployeeID
+
+END
+GO
+
+
